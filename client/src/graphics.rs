9a029a0b1b021214b6f2da48f298a1531e5ffb85
Reinert Lemmens <reilemx@gmail.com>
2022-07-16 13:51:29 +0200

    Attempt to refactor graphics code

--- client/src/graphics.rs
+++ client/src/graphics.rs
@@ -1,5 +1,10 @@
 use std::borrow::Cow;
+use wgpu::{
+    ColorTargetState, Device, PipelineLayout, PipelineLayoutDescriptor, Queue,
+    RenderPipelineDescriptor, ShaderModule, TextureFormat,
+};
 use winit::{
+    dpi::PhysicalSize,
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::Window,
@@ -8,74 +13,29 @@ use winit::{
 // EXAMPLE: FROM WGPU EXAMPLE HELLO_TRIANGLE
 
 pub async fn run_loop(event_loop: EventLoop<()>, window: Window) {
-    let size = window.inner_size();
-    let instance = wgpu::Instance::new(wgpu::Backends::all());
-    let surface = unsafe { instance.create_surface(&window) };
-    let adapter = instance
-        .request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::default(),
-            force_fallback_adapter: false,
-            // Request an adapter which can render to our surface
-            compatible_surface: Some(&surface),
-        })
-        .await
-        .expect("Failed to find an appropriate adapter");
-
+    // Create size, instance, surface & adapter
+    let (size, instance, surface, adapter) = init_adapter(&window).await;
     // Create the logical device and command queue
-    let (device, queue) = adapter
-        .request_device(
-            &wgpu::DeviceDescriptor {
-                label: None,
-                features: wgpu::Features::empty(),
-                // Make sure we use the texture resolution limits from the adapter, so we can support images the size of the swapchain.
-                limits: wgpu::Limits::downlevel_webgl2_defaults()
-                    .using_resolution(adapter.limits()),
-            },
-            None,
-        )
-        .await
-        .expect("Failed to create device");
+    let (device, queue) = get_device_queue(&adapter).await;
 
-    // Load the shaders from disk
-    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-        label: None,
-        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
-    });
+    // Load in the wgsl shader
+    let shader = load_wgsl_shader(&device, include_str!("shader.wgsl"));
 
-    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+    let swapchain_format = surface.get_supported_formats(&adapter)[0];
+
+    let pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
         label: None,
         bind_group_layouts: &[],
         push_constant_ranges: &[],
     });
 
-    let swapchain_format = surface.get_supported_formats(&adapter)[0];
+    let render_pipeline = device.create_render_pipeline(&create_render_pipeline_descriptor(
+        &shader,
+        &pipeline_layout,
+        &[Some(swapchain_format.into())],
+    ));
 
-    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        label: None,
-        layout: Some(&pipeline_layout),
-        vertex: wgpu::VertexState {
-            module: &shader,
-            entry_point: "vs_main",
-            buffers: &[],
-        },
-        fragment: Some(wgpu::FragmentState {
-            module: &shader,
-            entry_point: "fs_main",
-            targets: &[Some(swapchain_format.into())],
-        }),
-        primitive: wgpu::PrimitiveState::default(),
-        depth_stencil: None,
-        multisample: wgpu::MultisampleState::default(),
-        multiview: None,
-    });
-
-    let mut config = wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: swapchain_format,
-        width: size.width,
-        height: size.height,
-        present_mode: wgpu::PresentMode::Fifo,
-    };
+    let mut config = create_default_surface_config(&size, &swapchain_format);
 
     surface.configure(&device, &config);
 
@@ -83,7 +43,7 @@ pub async fn run_loop(event_loop: EventLoop<()>, window: Window) {
         // Have the closure take ownership of the resources.
         // `event_loop.run` never returns, therefore we must do this to ensure
         // the resources are properly cleaned up.
-        let _ = (&instance, &adapter, &shader, &pipeline_layout);
+        let _ = (&instance, &adapter, &shader);
 
         *control_flow = ControlFlow::Wait;
         match event {
@@ -134,4 +94,89 @@ pub async fn run_loop(event_loop: EventLoop<()>, window: Window) {
             _ => {}
         }
     });
+}
+
+async fn init_adapter(
+    window: &Window,
+) -> (
+    PhysicalSize<u32>,
+    wgpu::Instance,
+    wgpu::Surface,
+    wgpu::Adapter,
+) {
+    let size = window.inner_size();
+    let instance = wgpu::Instance::new(wgpu::Backends::all());
+    let surface = unsafe { instance.create_surface(&window) };
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            force_fallback_adapter: false,
+            // Request an adapter which can render to our surface
+            compatible_surface: Some(&surface),
+        })
+        .await
+        .expect("Failed to find an appropriate adapter");
+    (size, instance, surface, adapter)
+}
+
+async fn get_device_queue(adapter: &wgpu::Adapter) -> (Device, Queue) {
+    adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                features: wgpu::Features::empty(),
+                // Make sure we use the texture resolution limits from the adapter, so we can support images the size of the swapchain.
+                limits: wgpu::Limits::downlevel_webgl2_defaults()
+                    .using_resolution(adapter.limits()),
+            },
+            None,
+        )
+        .await
+        .expect("Failed to create device")
+}
+
+fn load_wgsl_shader(device: &Device, shader_path: &str) -> ShaderModule {
+    // Load the shaders from disk
+    device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: None,
+        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(shader_path)),
+    })
+}
+
+fn create_render_pipeline_descriptor<'a>(
+    shader: &'a ShaderModule,
+    pipeline_layout: &'a PipelineLayout,
+    targets: &'a [Option<ColorTargetState>],
+) -> RenderPipelineDescriptor<'a> {
+    RenderPipelineDescriptor {
+        label: None,
+        layout: Some(pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: shader,
+            entry_point: "vs_main",
+            buffers: &[],
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: shader,
+            entry_point: "fs_main",
+            targets,
+        }),
+        primitive: wgpu::PrimitiveState::default(),
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState::default(),
+        multiview: None,
+    }
+}
+
+fn create_default_surface_config(
+    size: &PhysicalSize<u32>,
+    swapchain_format: &TextureFormat,
+) -> wgpu::SurfaceConfiguration {
+    wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: *swapchain_format,
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::Fifo,
+    }
 }
