804a100fb09d1f8dfd29de9185f2361ad5871d1b
Reinert Lemmens <reilemx@gmail.com>
2022-08-06 15:21:06 +0200

    Grouping tile map in one entity

--- client/src/sprite.rs
+++ client/src/sprite.rs
@@ -2,24 +2,27 @@ use bevy::prelude::*;
 
 use crate::{texture::TextureSheet, TILE_SIZE};
 
+pub fn create_sprite(index: usize) -> TextureAtlasSprite {
+    let mut sprite = TextureAtlasSprite::new(index);
+    sprite.custom_size = Some(Vec2::splat(TILE_SIZE));
+    sprite
+}
+
 pub fn spawn_sprite(
     commands: &mut Commands,
     texture_sheet: &TextureSheet,
     index: usize,
     translation: Vec3,
 ) -> Entity {
-    let mut sprite = TextureAtlasSprite::new(index);
-    sprite.custom_size = Some(Vec2::splat(TILE_SIZE));
-
     commands
         .spawn_bundle(SpriteSheetBundle {
-            sprite,
+            sprite: create_sprite(index),
             texture_atlas: texture_sheet.atlas_handle.clone(),
             transform: Transform {
                 translation,
-                ..Default::default()
+                ..default()
             },
-            ..Default::default()
+            ..default()
         })
         .id()
 }
--- client/src/world.rs
+++ client/src/world.rs
@@ -1,6 +1,10 @@
 use bevy::prelude::*;
 
-use crate::{sprite::spawn_sprite, texture::TextureSheet, TILE_SIZE};
+use crate::{
+    sprite::{create_sprite, spawn_sprite},
+    texture::TextureSheet,
+    TILE_SIZE,
+};
 
 pub struct WorldPlugin;
 
@@ -10,26 +14,46 @@ impl Plugin for WorldPlugin {
     }
 }
 
-const WIDTH: f32 = 20.0;
-const HEIGHT: f32 = 15.0;
+const WIDTH: f32 = 9.0;
+const HEIGHT: f32 = 8.0;
 
 fn create_tile_background(mut commands: Commands, texture_sheet: Res<TextureSheet>) {
     // TODO: Make tile background dynamic based on screen size
-    // TODO: Add all tiles into one background entity
-    let translation_x = TILE_SIZE * (WIDTH / 2.0).floor();
-    let translation_y = TILE_SIZE * (HEIGHT / 2.0).floor();
+
+    let mut tiles = Vec::new();
+
     for y in 0..(HEIGHT as u32) {
         for x in 0..(WIDTH as u32) {
-            spawn_sprite(
-                &mut commands,
-                &texture_sheet,
-                0,
-                Vec3 {
-                    x: (x as f32) * TILE_SIZE - translation_x,
-                    y: (y as f32) * TILE_SIZE - translation_y,
-                    z: 0.0,
-                },
-            );
+            let tile = spawn_sprite(&mut commands, &texture_sheet, 0, tile_location(x, y));
+            tiles.push(tile);
         }
+    }
+
+    let mut parent_tile = TextureAtlasSprite::new(0);
+    parent_tile.custom_size = Some(Vec2::splat(TILE_SIZE));
+
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            sprite: create_sprite(0),
+            texture_atlas: texture_sheet.atlas_handle.clone(),
+            transform: Transform {
+                translation: tile_location(0, 0), // TODO: Remove duplicate parent tile somehow
+                ..default()
+            },
+            ..default()
+        })
+        .insert(Name::new("Map"))
+        .insert(Transform::default())
+        .insert(GlobalTransform::default())
+        .push_children(&tiles);
+}
+
+fn tile_location(x: u32, y: u32) -> Vec3 {
+    let translation_x = TILE_SIZE * (WIDTH / 2.0).floor();
+    let translation_y = TILE_SIZE * (HEIGHT / 2.0).floor();
+    Vec3 {
+        x: (x as f32) * TILE_SIZE - translation_x,
+        y: (y as f32) * TILE_SIZE - translation_y,
+        z: 0.0,
     }
 }
