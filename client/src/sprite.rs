c83dea7dd35a3bd7c51457f7cddc9f6412a7bca8
Reinert Lemmens <reilemx@gmail.com>
2022-08-07 16:19:06 +0200

    Refactor world & sprite creation

--- client/src/sprite.rs
+++ client/src/sprite.rs
@@ -1,15 +1,40 @@
-use bevy::prelude::*;
+use bevy::{ecs::system::EntityCommands, prelude::*};
 
 use crate::{texture::TextureSheet, TILE_SIZE};
 
 pub fn create_sprite(index: usize) -> TextureAtlasSprite {
-    create_sprite_with_size(index, TILE_SIZE)
+    create_sprite_with_size(SpriteInfo {
+        index,
+        size: TILE_SIZE,
+    })
 }
 
-pub fn create_sprite_with_size(index: usize, size: f32) -> TextureAtlasSprite {
-    let mut sprite = TextureAtlasSprite::new(index);
-    sprite.custom_size = Some(Vec2::splat(size));
+pub struct SpriteInfo {
+    pub index: usize,
+    pub size: f32,
+}
+
+pub fn create_sprite_with_size(info: SpriteInfo) -> TextureAtlasSprite {
+    let mut sprite = TextureAtlasSprite::new(info.index);
+    sprite.custom_size = Some(Vec2::splat(info.size));
     sprite
+}
+
+pub fn spawn_bundle<'w, 's, 'a>(
+    commands: &'a mut Commands<'w, 's>,
+    texture_sheet: &TextureSheet,
+    sprite: TextureAtlasSprite,
+    translation: Vec3,
+) -> EntityCommands<'w, 's, 'a> {
+    commands.spawn_bundle(SpriteSheetBundle {
+        sprite,
+        texture_atlas: texture_sheet.atlas_handle.clone(),
+        transform: Transform {
+            translation,
+            ..default()
+        },
+        ..default()
+    })
 }
 
 pub fn spawn_sprite(
@@ -18,15 +43,21 @@ pub fn spawn_sprite(
     index: usize,
     translation: Vec3,
 ) -> Entity {
-    commands
-        .spawn_bundle(SpriteSheetBundle {
-            sprite: create_sprite(index),
-            texture_atlas: texture_sheet.atlas_handle.clone(),
-            transform: Transform {
-                translation,
-                ..default()
-            },
-            ..default()
-        })
-        .id()
+    spawn_bundle(commands, texture_sheet, create_sprite(index), translation).id()
+}
+
+pub fn spawn_sprite_with_size(
+    commands: &mut Commands,
+    texture_sheet: &TextureSheet,
+    index: usize,
+    size: f32,
+    translation: Vec3,
+) -> Entity {
+    spawn_bundle(
+        commands,
+        texture_sheet,
+        create_sprite_with_size(SpriteInfo { index, size }),
+        translation,
+    )
+    .id()
 }
--- client/src/world.rs
+++ client/src/world.rs
@@ -1,10 +1,6 @@
 use bevy::prelude::*;
 
-use crate::{
-    sprite::{create_sprite_with_size, spawn_sprite},
-    texture::TextureSheet,
-    TILE_SIZE,
-};
+use crate::{sprite::*, texture::TextureSheet, TILE_SIZE};
 
 pub struct WorldPlugin;
 
@@ -28,31 +24,35 @@ fn create_tile_background(mut commands: Commands, texture_sheet: Res<TextureShee
 
     for y in 0..(HEIGHT as u32) {
         for x in 0..(WIDTH as u32) {
-            let tile = spawn_sprite(&mut commands, &texture_sheet, 0, tile_location(x, y));
+            let tile = spawn_sprite(
+                &mut commands,
+                &texture_sheet,
+                0,
+                tile_location(UVec2 { x, y }),
+            );
             tiles.push(tile);
         }
     }
 
-    commands
-        .spawn_bundle(SpriteSheetBundle {
-            sprite: create_sprite_with_size(0, 0.0), // Set size to zero to hide parent tile texture
-            texture_atlas: texture_sheet.atlas_handle.clone(),
-            transform: Transform {
-                translation: tile_location(0, 0),
-                ..default()
-            },
-            ..default()
-        })
-        .insert(Name::new("Map"))
-        .insert(Transform::default())
-        .insert(GlobalTransform::default())
-        .push_children(&tiles);
+    spawn_bundle(
+        &mut commands,
+        &texture_sheet,
+        create_sprite_with_size(SpriteInfo {
+            index: 0,
+            size: 0.0,
+        }),
+        tile_location(UVec2 { x: 0, y: 0 }),
+    )
+    .insert(Name::new("Map"))
+    .insert(Transform::default())
+    .insert(GlobalTransform::default())
+    .push_children(&tiles);
 }
 
-fn tile_location(x: u32, y: u32) -> Vec3 {
+fn tile_location(position: UVec2) -> Vec3 {
     Vec3 {
-        x: (x as f32) * TILE_SIZE - TRANSLATE_X,
-        y: (y as f32) * TILE_SIZE - TRANSLATE_Y,
+        x: (position.x as f32) * TILE_SIZE - TRANSLATE_X,
+        y: (position.y as f32) * TILE_SIZE - TRANSLATE_Y,
         z: 0.0,
     }
 }
